e0b37509bfa8e3eae010f8de4fc25045304461c9
lp123 <1970664102@qq.com>
2024-01-23 16:16:26 +0800

    feat:组件子应用

--- scripts/webpack.base.js
+++ scripts/webpack.base.js
@@ -1,89 +1,94 @@
-
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const { name } = require('../package.json');
 
 module.exports = {
-    entry: '/src/App.tsx',
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: "js/[name][hash:6].js",
-        // 本地BrowserRouter 配置将请求路径转发的 index.html
-        publicPath: '/',
+  entry: '/src/App.tsx',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name][hash:6].js',
+    // 本地BrowserRouter 配置将请求路径转发的 index.html
+    publicPath: '/',
+    // qiankun子应用配置
+    library: `${name}-[name]`,
+    libraryTarget: 'umd',
+    chunkLoadingGlobal: `webpackJsonp_${name}`
+  },
+  // 配置如何解析
+  resolve: {
+    // 别名配置
+    alias: {
+      // 方便导入
+      '@': path.resolve(__dirname, '../src/')
     },
-    // 配置如何解析
-    resolve: {
-        // 别名配置
-        alias: {
-            // 方便导入
-            '@': path.resolve(__dirname, '../src/'),
-        },
-        // 导入不写后缀时，默认按此规则解析
-        extensions: ['.tsx', '.ts', '.js', '.less', '.json']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.PUBLIC_PATH': "'/public'",
-        }),
-        new MiniCssExtractPlugin({
-            filename: '../assets/[name][hash:6].css'
-        }),
-        new htmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, '../public/index.html'),
-        })
-    ],
+    // 导入不写后缀时，默认按此规则解析
+    extensions: ['.tsx', '.ts', '.js', '.less', '.json']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.PUBLIC_PATH': "'/public'"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '../assets/[name][hash:6].css'
+    }),
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ],
 
-    module: {
-        rules: [
-            {
-                test: /\.(t|j)sx?$/,//ts、tsx 都匹配
-                use: ['babel-loader'],
-                exclude: '/node_modules/',
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: "css-loader",
-                        options: {
-                            // 开启less 模块化使用 styles.red 使用方式
-                            modules: {
-                                localIdentName: '[local]_[hash:base64:5]',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                // 自动添加css前缀
-                                plugin: ['postcss-preset-env']
-                            },
-
-                        }
-                    },
-                    'less-loader'],
-            },
-            {
-                test: /\.(svg|png|gif|\.jpe?g)$/,
-                // use:'asset/inline',//以base64 方式导出
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name][hash:6].[ext]',
-                        // 图片输出目录
-                        outputPath: 'assets',
-                    }
-                }],
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/,//字体处理
-                use: 'asset/resource',//以文件方式导出
-            },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/, //ts、tsx 都匹配
+        use: ['babel-loader'],
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // 开启less 模块化使用 styles.red 使用方式
+              modules: {
+                localIdentName: '[local]_[hash:base64:5]'
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                // 自动添加css前缀
+                plugin: ['postcss-preset-env']
+              }
+            }
+          },
+          'less-loader'
         ]
-    },
-
-}
+      },
+      {
+        test: /\.(svg|png|gif|\.jpe?g)$/,
+        type: 'asset/resource' //以base64 方式导出
+        // use: [
+        //   {
+        //     loader: 'url-loader',
+        //     options: {
+        //       name: '[name][hash:6].[ext]',
+        //       // 图片输出目录
+        //       outputPath: 'assets'
+        //     }
+        //   }
+        // ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/, //字体处理
+        type: 'asset/resource' //以文件方式导出
+      }
+    ]
+  }
+};
--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -1,7 +1,6 @@
-
 const path = require('path');
-const { merge } = require('webpack-merge')
-const portfinder = require('portfinder')
+const { merge } = require('webpack-merge');
+const portfinder = require('portfinder');
 const baseConfig = require('./webpack.base');
 const { PORT } = require('./utils/constant');
 
@@ -10,36 +9,37 @@ const { PORT } = require('./utils/constant');
 portfinder.basePort = PORT;
 
 const devConfig = {
-    mode: 'development',
-    // 配置本地服务
-    devServer: {
-        hot: true,
-        port: PORT,
-        open: true,
-        compress: true,//压缩代码
-        // 本地BrowserRouter 配置将请求路径转发的 index.html
-        historyApiFallback: true,
-        proxy: {
-            "/api": {
-                target: 'https://www.baidu.com',
-                pathRewrite: {
-                    '^/api': '',
-                },
-                changeOrigin: true,
-
-            }
-        }
+  mode: 'development',
+  // 配置本地服务
+  devServer: {
+    hot: true,
+    port: PORT,
+    open: true,
+    compress: true, //压缩代码
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    // 本地BrowserRouter 配置将请求路径转发的 index.html
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'https://www.baidu.com',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
     }
-
-}
+  }
+};
 
 module.exports = async function () {
-    try {
-        // 如果当前端口被占用，则自动返回下一个端口
-        const port = await portfinder.getPortPromise();
-        devConfig.devServer.port = port;
-        return merge(devConfig, baseConfig)
-    } catch (error) {
-        throw new Error(error)
-    }
-}
+  try {
+    // 如果当前端口被占用，则自动返回下一个端口
+    const port = await portfinder.getPortPromise();
+    devConfig.devServer.port = port;
+    return merge(devConfig, baseConfig);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
